8b161a5deed3d453a67de12bce85bc855c08eb24
Kurylovich Artsiom <tiomakk@gmail.com>
2018-10-02 20:52:35 +0300

    Added webapi for Alisa

---
+++ Masha/Controllers/AlisaController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Masha.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Masha.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlisaController : ControllerBase
+    {
+        [HttpPost("")]
+        public void ProcessRequest([FromBody] AlisaRequestModel request)
+        {
+
+        }
+    }
+}
---
+++ Masha/Models/AlisaRequestModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Masha.Models
+{
+    public class AlisaRequestModel
+    {
+        public Meta Meta { get; set; }
+        public Request Request { get; set; }
+        public Session Session { get; set; }
+        public string Version { get; set; }
+    }
+
+    public class Meta
+    {
+        public string Locale { get; set; }
+        public string Timezone { get; set; }
+        public string ClientId { get; set; }
+    }
+
+    public class Request
+    {
+        public string Command { get; set; }
+        public string OriginalUtterance { get; set; }
+        public string Type { get; set; }
+        public Markup Markup { get; set; }
+    }
+
+    public class Markup
+    {
+        public bool DangerousContext { get; set; }
+    }
+
+    public class Session
+    {
+        public bool New { get; set; }
+        public int MessageId { get; set; }
+        public string SessionId { get; set; }
+        public string SkillId { get; set; }
+        public string UserId { get; set; }
+    }
+}
